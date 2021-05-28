1f0ef7cb46caa3c1039ca751b3a7e5a0560c2d19
Nico Franke <nico.franke01@gmail.com>
2021-05-28 20:29:17 +0200

    Create initial register page

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,15 +1,124 @@
 <template>
-  <p>Memota</p>
-  <router-view />
+  <q-layout view="hHh LpR fFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn flat round icon="menu" />
+        <q-toolbar-title>Memota</q-toolbar-title>
+        <q-btn stretch flat label="Login" @click="login = true" />
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+  <q-dialog v-model="login" persistent>
+    <q-card class="login">
+      <q-tab-panels v-model="tab" animated>
+        <q-tab-panel class="bg-primary" name="login">
+          <div class="text-h6 text-white">Login</div>
+        </q-tab-panel>
+        <q-tab-panel class="bg-primary" name="register">
+          <div class="text-h6 text-white">Register</div>
+        </q-tab-panel>
+      </q-tab-panels>
+      <q-tabs
+        v-model="tab"
+        dense
+        class="text-grey"
+        active-color="primary"
+        indicator-color="primary"
+        align="justify"
+        narrow-indicator
+      >
+        <q-tab name="login" label="Login" />
+        <q-tab name="register" label="Register" />
+      </q-tabs>
+      <q-separator />
+      <q-tab-panels v-model="tab" animated>
+        <q-tab-panel name="login"> </q-tab-panel>
+        <q-tab-panel name="register">
+          <q-form class="q-gutter-md">
+            <q-input
+              v-model="email"
+              filled
+              type="email"
+              label="Email"
+              lazy-rules
+              :rules="[
+                (val) => val.length <= 64 || 'Email must be 64 characters or less',
+                (val) =>
+                  new RegExp('^([A-Za-z0-9_\\-.])+@([A-Za-z0-9_\\-.])+\\.([A-Za-z]{2,15})$').test(val) ||
+                  'Mail address must be valid',
+              ]"
+            />
+            <q-input
+              v-model="username"
+              filled
+              label="Username"
+              lazy-rules
+              :rules="[
+                (val) => (val && val.length > 0) || 'Please enter your username',
+                (val) => val.length <= 32 || 'Username must be 32 characters or less',
+                (val) => val.length >= 3 || 'Username must be at least 3 characters',
+                (val) => new RegExp('^[\\w_]+$', 'g').test(val) || 'Username must only contain numbers, letters and _',
+              ]"
+            />
+            <q-input
+              v-model="password"
+              filled
+              :type="showPwd ? 'text' : 'password'"
+              label="Password"
+              lazy-rules
+              :rules="[
+                (val) => (val && val.length > 0) || 'Please enter your password',
+                (val) => val.length <= 64 || 'Password must be 64 characters or less',
+                (val) => val.length >= 5 || 'Username must be at least 5 characters',
+              ]"
+            >
+              <template #append>
+                <q-icon
+                  :name="showPwd ? 'visibility' : 'visibility_off'"
+                  class="cursor-pointer"
+                  @click="showPwd = !showPwd"
+                />
+              </template>
+            </q-input>
+            <div>
+              <q-btn label="Register" type="submit" color="primary" />
+              <q-btn label="Cancel" type="reset" color="primary" flat @click="login = false" class="q-ml-sm" />
+            </div>
+          </q-form>
+        </q-tab-panel>
+      </q-tab-panels>
+    </q-card>
+  </q-dialog>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue"
+import { defineComponent, ref } from "vue"
 
 export default defineComponent({
   name: "MainLayout",
   setup() {
-    return {}
+    return {
+      login: ref(false),
+      tab: ref("login"),
+      showPwd: ref(false),
+      email: ref(""),
+      username: ref(""),
+      password: ref(""),
+    }
   },
 })
 </script>
+
+<style lang="scss" scoped>
+.login {
+  width: 50%;
+}
+@media (max-width: $breakpoint-sm-max) {
+  .login {
+    width: 90%;
+  }
+}
+</style>
